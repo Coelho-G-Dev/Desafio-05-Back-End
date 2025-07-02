aff1a5721e6d2dfe1ef9467403a14108af2250d1
Coelho <gabrielbiellosousa@gmail.com>
2025-07-02 00:42:51 -0300

    a

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,14 +1,6 @@
 const whitelist = [
-  'https://buscasusma-e9idvek5v-pytwers-projects.vercel.app/', //front-end
-  'https://buscasusma-e9idvek5v-pytwers-projects.vercel.app', 
-  'https://buscasusma-b711ua4mc-pytwers-projects.vercel.app/',              // Para testes locais 
-  'https://buscasusma-bt1fubdt2-pytwers-projects.vercel.app/',
-  'https://buscasusma-5ccgwauyy-pytwers-projects.vercel.app/',              // Para testes locais 
-  'https://buscasusma-b711ua4mc-pytwers-projects.vercel.app/',  
-  'https://buscasusma-lllb9440s-pytwers-projects.vercel.app/',
-  'https://buscasusma-7pgtj8a5d-pytwers-projects.vercel.app/' ,
-  'https://buscasusma-b711ua4mc-pytwers-projects.vercel.app/jogar',   
-  'https://buscasusma-b711ua4mc-pytwers-projects.vercel.app/',       // Para testes locais ,       
+  'https://buscasusma-7pgtj8a5d-pytwers-projects.vercel.app/',       // Para testes locais ,       
+  '',
   'https://desafio-05-api.onrender.com' // URL do deploy no Render
 ];
 const corsOptions = {
